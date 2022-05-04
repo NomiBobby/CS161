e7ede5331cc6e26bc0b6c4bd484f84d8553d790a
NomiBobby <maoqiwei1014@gmail>
2022-05-04 00:31:26 -0700

    final qsort with comparison count

--- L3/qsort.py
+++ L3/qsort.py
@@ -3,17 +3,17 @@ import math
 
 
 # Read input file into array
-with open('QuickSort1.txt', 'r') as f:
+with open('QuickSort.txt', 'r') as f:
     array = []
     input = f.read().splitlines()
     for x in input:
         array.append(int(x))
 
-print("The original array is:", array)
-print("------------------------------------")
+# print("The original array is:", array)
+#print("------------------------------------")
 
 # Partition Mode: # Mode 0, use first element, # Mode 1, use final element, # Mode 2 "median-of-three" pivot rule
-mode = 0
+mode = 2
 
 # Define quick sort function
 def qsort(X):
@@ -21,9 +21,13 @@ def qsort(X):
     # If array length is 1 return array
     X_len = len(X)
     if X_len <= 1:
-        return X
+        m = 0
+        return X, m
     
     else:
+        
+        m = len(X) - 1
+        
         p_index = ChoosePivot(X, mode)
         
         # Preprocessing: swap pivot with the first element
@@ -42,15 +46,20 @@ def qsort(X):
             # else do nothing
         # swap pivot to the middle
         X[l], X[i - 1] = X[i - 1], X[l]
-        print("The partitioned array =", X)
+        #print("The partitioned array =", X)
         
         # Recursive call
-        print("length of array = ", len(X), "i = ", i, "j = ", j)
+        #print("length of array = ", len(X), "i = ", i, "j = ", j)
         
-        print(X[:i - 1])
-        print(X[i:])
-        X_sorted = qsort(X[:i - 1]) + [X[i-1]] + qsort(X[i:])
-        return X_sorted
+        #print(X[:i - 1])
+        #print(X[i:])
+        X_l = qsort(X[:i - 1])
+        X_r = qsort(X[i:])
+        X_sorted = X_l[0] + [X[i-1]] + X_r[0]
+        
+        m = m + int(X_l[1]) + int(X_r[1])
+        
+        return X_sorted, m
     
     
 # Define ChoosePivot, return the index of the pivot
@@ -74,11 +83,14 @@ def ChoosePivot(X, mode):
 def MedianOfThree(X):
     X_len = len(X) # >= 2
     a = X[0]
+    print("X_len = ", X_len)
     if X_len % 2 == 0:
-        b_index = X_len / 2 - 1
+        b_index = int(X_len / 2 - 1)
     else:
-        b_index = [X_len - 1] / 2
-    b = [b_index]
+        b_index = int((X_len - 1) / 2)
+    print("b_index = ", b_index)
+    b = X[b_index]
+    print(type(b))
     c = X[-1]
     
     if a < b:
@@ -114,4 +126,5 @@ def Partition(X, l, r):
     return X
 
 array_sorted = qsort(array)
-print("The sorted array is: ", array_sorted)
+# print("The sorted array is: ", array_sorted[0])
+print("Number of comparisons is:", array_sorted[1])
